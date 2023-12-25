f7a030a753ccd6c52648d5fde3609395939c3892
Arian <Arianbod@outlook.com>
2023-12-25 14:51:37 +0300

    useGlobalContext testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { useGlobalContext } from './Context';
 
 function App() {
 	const [count, setCount] = useState(0);
-
-	return <>hello</>;
+	const { nameState, setNameState } = useGlobalContext();
+	setNameState('Jason');
+	console.log(nameState);
+	return <>{nameState}</>;
 }
 
 export default App;
